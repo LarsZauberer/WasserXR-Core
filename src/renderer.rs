58d42e8a795f239ef914c7baecb505f01d4728ba
LarsZauberer <wasser.ian@gmail.com>
2026-06-29 15:11:51 +0200

    Renderer: Transform rendering added

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,5 @@
-use glium::{Program, Surface, uniform};
+use glam::{EulerRot, Mat3, Mat4, Quat, Vec3, camera::rh::proj::opengl::perspective};
+use glium::{Program, Surface, dynamic_uniform, winit::window::Window};
 use wasserxr::{Uuid, scene::Scene, system, warn};
 
 use crate::{model_asset::Mesh, window::component::Display};
@@ -18,7 +19,9 @@ fn renderer(scene: &mut Scene, entities: Vec<Vec<Uuid>>) {
     let camera_entity = entities[1][0];
 
     // Getting the OpenGL Context
-    let Ok((display,)) = scene.query::<(&Display,)>(window_entity, "Window", &["display"]) else {
+    let Ok((display, window)) =
+        scene.query::<(&Display, &Window)>(window_entity, "Window", &["display", "window"])
+    else {
         warn!(
             scene,
             "Failed to get the OpenGL context from the Window entity: {}", window_entity
@@ -37,11 +40,28 @@ fn renderer(scene: &mut Scene, entities: Vec<Vec<Uuid>>) {
         return;
     };
 
-    let mut frame = display.draw();
+    let aspect_ratio: f32 =
+        (window.inner_size().width as f32) / (window.inner_size().height as f32);
 
+    // Camera position
+    let mut cam_position: [f32; 3] = [0.0, 0.0, 0.0];
+    let mut cam_rotation: [f32; 3] = [0.0, 0.0, 0.0];
+    let fov = fov.to_radians();
+    let near = *near;
+    let far = *far;
+
+    if let Ok((position, rotation)) =
+        scene.query::<(&[f32; 3], &[f32; 3])>(camera_entity, "Transform", &["position", "rotation"])
+    {
+        cam_position = *position;
+        cam_rotation = *rotation;
+    }
+
+    let mut frame = display.draw();
     frame.clear_color(0.0, 0.0, 0.0, 1.0);
 
     for entity in &entities[2] {
+        // Get all assets and information
         let Ok((model_path, shader_path)) =
             scene.query::<(&String, &String)>(*entity, "Model", &["model", "shader"])
         else {
@@ -81,8 +101,48 @@ fn renderer(scene: &mut Scene, entities: Vec<Vec<Uuid>>) {
             continue;
         };
 
-        let uniforms = uniform! {};
+        let (transform, normal_transform) = scene
+            .query::<(&[f32; 3], &[f32; 3], &[f32; 3])>(
+                *entity,
+                "Transform",
+                &["position", "rotation", "scale"],
+            )
+            .ok()
+            .map(|(position, rotation, scale)| {
+                // Compute Model Transform
+                let translation = Vec3::from_array(*position);
+                let scale = Vec3::from_array(*scale);
+                let rotation = make_quat(*rotation);
+                let model_transform =
+                    Mat4::from_scale_rotation_translation(scale, rotation, translation);
 
+                // Compute View Transform
+                let translation = Vec3::from_array(cam_position);
+                let rotation = make_quat(cam_rotation);
+                let view_transform = Mat4::from_rotation_translation(rotation, translation);
+
+                // Compute Projection Transform
+                let projection_transform = perspective(fov, aspect_ratio, near, far);
+
+                // Show compute full transformation
+                let mvp = projection_transform * view_transform * model_transform;
+                let normal_transform = Mat3::from_mat4(model_transform).inverse().transpose();
+
+                (mvp.to_cols_array_2d(), normal_transform.to_cols_array_2d())
+            })
+            .unwrap_or_else(|| {
+                (
+                    Mat4::IDENTITY.to_cols_array_2d(),
+                    Mat3::IDENTITY.to_cols_array_2d(),
+                )
+            });
+
+        // Build Uniforms
+        let mut uniforms = dynamic_uniform! {};
+        uniforms.add("transform", &transform);
+        uniforms.add("normal_transform", &normal_transform);
+
+        // Final draw calls
         for mesh in meshes {
             match frame.draw(
                 &mesh.vertices,
@@ -101,4 +161,17 @@ fn renderer(scene: &mut Scene, entities: Vec<Vec<Uuid>>) {
     }
 
     frame.finish().unwrap();
+}
+
+fn to_rotation_vec3_from_array(vec: [f32; 3]) -> Vec3 {
+    Vec3::new(
+        vec[0].to_radians(),
+        vec[1].to_radians(),
+        vec[2].to_radians(),
+    )
+}
+
+fn make_quat(vec: [f32; 3]) -> Quat {
+    let rotation = to_rotation_vec3_from_array(vec);
+    Quat::from_euler(EulerRot::XYZ, rotation.x, rotation.y, rotation.z)
 }
